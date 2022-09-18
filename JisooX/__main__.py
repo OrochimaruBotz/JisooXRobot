6f5a8162a5a536e9208c2ae2c590e857e7432c38
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-09-18 18:11:46 +0700

    Update __main__.py

--- JisooX/__main__.py
+++ JisooX/__main__.py
@@ -73,7 +73,7 @@ def vercheck() -> str:
 
 SOURCE_STRING = """
 ⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/feriexp/JisooXRobot)
-⚡You Can Clone Me [Here](https://heroku.com/deploy?template=https://github.com/feriexp/JisooXRobot.git)
+⚡You Can Clone Me [Here](https://heroku.com/deploy?template=https://github.com/OrochimaruBotz/JisooXRobot.git)
 """
 
 
@@ -92,7 +92,7 @@ GDPR = []
 
 START_IMG = os.environ.get('START_IMG', None)
 if START_IMG is None:
-    img = "https://telegra.ph/file/7b375b3abb0b483fd0d98.jpg"
+    img = "https://telegra.ph/file/0125e8e8815bd3d495029.jpg"
 else:
   img = START_IMG    
     
@@ -193,8 +193,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="[► Help ◄]",callback_data="help_back"),InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/xflicks")]]
-    keyboard += [[InlineKeyboardButton(text="[► Source ◄]",url="https://github.com/FeriEXP/JisooXRobot"),InlineKeyboardButton(text="[► Add Me ◄]",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="[► Help ◄]",callback_data="help_back"),InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/BijiKacang")]]
+    keyboard += [[InlineKeyboardButton(text="[► Source ◄]",url="https://github.com"),InlineKeyboardButton(text="[► Add Me ◄]",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
@@ -295,7 +295,7 @@ def get_help(bot: Bot, update: Update):
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="[► Help ◄]",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/xflicks")]]))
+                                                [InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/BijiKacang")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
@@ -615,7 +615,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("JisooXRobot running...")
+        LOGGER.info("GojoRobot running...")
         updater.start_polling(timeout=15, read_latency=4)
         
     if len(argv) not in (1, 3, 4):
